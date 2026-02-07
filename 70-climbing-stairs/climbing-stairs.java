aed4050469676fb2fc4ff90882033d56025f8b20
Raksha-1225 <sriraksha5555@gmail.com>
2026-02-07 22:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (69.84%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n<=2) {
+            return n;
+        }
+        int a=1;  
+        int b=2; 
+        int t=0;
+        for (int i=3;i<=n;i++) {
+            t=a+b;
+            a=b;
+            b=t;
+        }
+        return t;
+    }
+}
