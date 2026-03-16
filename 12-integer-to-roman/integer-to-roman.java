9c67762bbf874aef01e657359e264069aebcfb4a
Raksha-1225 <sriraksha5555@gmail.com>
2026-03-16 12:18:58 +0530

    Time: 8 ms (6.76%) | Memory: 47.4 MB (5.29%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,14 +1,26 @@
 class Solution {
     public String intToRoman(int num) {
-        String[] thou={"","M","MM","MMM"};
-        String[] hund={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String[] tens={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String[] ones={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        Map<Integer,String> map=new LinkedHashMap<>();
+        map.put(1000,"M");
+        map.put(900,"CM");
+        map.put(500,"D");
+        map.put(400,"CD");
+        map.put(100,"C");
+        map.put(90,"XC");
+        map.put(50,"L");
+        map.put(40,"XL");
+        map.put(10,"X");
+        map.put(9,"IX");
+        map.put(5,"V");
+        map.put(4,"IV");
+        map.put(1,"I");
         String res="";
-        res=res+thou[num/1000];
-        res=res+hund[(num%1000)/100];
-        res=res+tens[(num%100)/10];
-        res=res+ones[num%10];
+        for(int key:map.keySet()){
+            while(num>=key){
+                res+=map.get(key);
+                num=num-key;
+            }
+        }
         return res;
     }
 }
