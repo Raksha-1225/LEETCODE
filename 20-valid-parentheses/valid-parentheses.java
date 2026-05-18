5a63ad29ce9ea73cbb222b100b753d0da64e230f
Sri Raksha <sriraksha5555@gmail.com>
2026-05-18 22:10:43 +0530

    Time: 3 ms (87.88%) | Memory: 43.4 MB (40.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,26 +3,25 @@ class Solution {
         if(s.length()%2!=0){
             return false;
         }
+
         Stack<Character> st=new Stack<>();
+
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            if((c=='('||c=='{'||c=='[')){
+
+            if(c=='('||c=='{'||c=='['){
                 st.push(c);
             }
-            else if(!st.isEmpty() && (
-        (c==')' && st.peek()=='(') ||
-        (c=='}' && st.peek()=='{') ||
-        (c==']' && st.peek()=='['))){
-            st.pop();
+
+            else if(!st.isEmpty()&&((c==')'&&st.peek()=='(')||(c=='}'&&st.peek()=='{')||(c==']'&&st.peek()=='['))){
+                st.pop();
             }
-            else if(st.isEmpty()&&(c==')'||c==']'||c=='}')){
-                return false;
-            }
+
             else{
                 return false;
             }
         }
+
         return st.isEmpty();
-        
     }
 }
