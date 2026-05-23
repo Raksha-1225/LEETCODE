34cec9477859d9983badfb524c69410bff435623
Sri Raksha <sriraksha5555@gmail.com>
2026-05-23 23:14:27 +0530

    Time: 2 ms (72.77%) | Memory: 45.1 MB (13.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
-    public List<String> res=new ArrayList<>();
+    public List<String> result=new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        gen(0,0,"",n);
-        return res;
+        generate(0,0,"",n);
+        return result;
     }
-    public void gen(int op,int cl,String s,int n){
+    public void generate(int op,int cl,String s,int n){
         if(s.length()==n*2){
-            res.add(s);
+            result.add(s);
             return;
         }
         if(op<n){
-            gen(op+1,cl,s+"(",n);
+            generate(op+1,cl,s+"(",n);
         }
         if(cl<op){
-            gen(op,cl+1,s+")",n);
+            generate(op,cl+1,s+")",n);
         }
     }
 }
