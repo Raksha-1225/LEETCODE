c83c91960c737d54e235214815098f6c422575c8
Sri Raksha <sriraksha5555@gmail.com>
2026-06-10 21:37:09 +0530

    Time: 5 ms (14.57%) | Memory: 43.4 MB (40.08%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
+
         if(s.length()%2!=0){
             return false;
         }
 
         Stack<Character> st=new Stack<>();
-
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-
-            if(c=='('||c=='{'||c=='['){
-                st.push(c);
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
             }
-
-            else if(!st.isEmpty()&&((c==')'&&st.peek()=='(')||(c=='}'&&st.peek()=='{')||(c==']'&&st.peek()=='['))){
+            else if(!st.isEmpty()&&((ch=='}'&&st.peek()=='{')||(ch==')'&&st.peek()=='(')||(ch==']'&&st.peek()=='['))){
                 st.pop();
             }
-
             else{
                 return false;
             }
