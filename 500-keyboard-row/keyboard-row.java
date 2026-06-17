4a2eff40e2d973cd04c8fd8abcfe8e35fe62b4ba
Sri Raksha <sriraksha5555@gmail.com>
2026-06-17 17:49:15 +0530

    Time: 1 ms (33.15%) | Memory: 43.1 MB (14.55%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,12 +1,10 @@
 class Solution {
-   
-    static String s1="qwertyuiop";
-    static String s2="asdfghjkl";
-    static String s3="zxcvbnm";
+    
+
     public static void row1(String s,ArrayList<String> al){
         for(int i=0;i<s.length();i++){
             String c=""+Character.toLowerCase(s.charAt(i));
-            if(s1.contains(c)){
+            if( "qwertyuiop".contains(c)){
                 continue;
             }
             return;
@@ -17,7 +15,7 @@ class Solution {
     public static void row2(String s,ArrayList<String> al){
         for(int i=0;i<s.length();i++){
             String c=""+Character.toLowerCase(s.charAt(i));
-            if(s2.contains(c)){
+            if("asdfghjkl".contains(c)){
                 continue;
             }
             return;
@@ -29,7 +27,7 @@ class Solution {
     public static void row3(String s,ArrayList<String> al){
         for(int i=0;i<s.length();i++){
             String c=""+Character.toLowerCase(s.charAt(i));
-            if(s3.contains(c)){
+            if("zxcvbnm".contains(c)){
                 continue;
             }
             return;
@@ -41,10 +39,10 @@ class Solution {
     public String[] findWords(String[] words) {
         ArrayList<String> al=new ArrayList<>();
         for(int i=0;i<words.length;i++){
-            if(s1.contains(Character.toLowerCase(words[i].charAt(0))+"")){
+            if("qwertyuiop".contains(Character.toLowerCase(words[i].charAt(0))+"")){
                 row1(words[i],al);
             }
-            else if(s2.contains(Character.toLowerCase(words[i].charAt(0))+"")){
+            else if("asdfghjkl".contains(Character.toLowerCase(words[i].charAt(0))+"")){
                 row2(words[i],al);
             }
             else{
